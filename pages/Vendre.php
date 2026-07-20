947d5b349b7e4002fa455b54fbe3285256b25c1e
vahinaaina-droid <vahinaainanirina@gmail.com>
2026-07-20 13:56:26 +0300

    next

--- pages/Vendre.php
+++ pages/Vendre.php
@@ -14,15 +14,19 @@
 <div class="body">
     <h1>Vendre des produits</h1>
     <div class=form>
+        <button><a class="btn" href="Acceuil.php?id_membre=<?=$_GET['id_membre']?>">Tout produit vendus</a></button>
+        <button><a class="btn" href="Mes_Ventes.php?id_membre=<?=$_GET['id_membre']?>">Mes produit vendus</a></button>
         <form action="traitement_produit.php" method="post">
-            <button><a class="btn" href="Acceuil.php?id_membre=<?=$_GET['id_membre']?>">Tout produit vendus</a></button>
-            <button><a class="btn" href="Mes_Ventes.php?id_membre=<?=$_GET['id_membre']?>">Mes produit vendus</a></button>
-            <p>Produit <select name="id_produit">
+            <label for="produit">Nom du produit</label>
+            <p><select name="id_produit" id="produit">
                 <? foreach($all_produit as $produit){?>
                     <option value="<?= $produit['id_produit']?>"><?= $produit['nom']?></option>
                     <?}?>
                 </select></p>
-                <p>Quantite: <input type="text" name="quantite"></p>
+                <label for="quantite">Quantite du produit a vendre</label>
+                <p><input type="text" name="quantite" id="quantite"></p>
+                <label for="fichier">Ajouter une image</label>
+                <input type="file" name="imgplat" id="fichier">
                 <p><input type="submit" value="Vendre"></p>
                 <input type="hidden" name="id_membre" value="<?=$_GET['id_membre']?>">
             </form>
