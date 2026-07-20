d8b421749c9b3b6fb2bbf5bf95adc99e076c792a
vahinaaina-droid <vahinaainanirina@gmail.com>
2026-07-20 11:45:08 +0300

    func

--- pages/achatproduit.php
+++ pages/achatproduit.php
@@ -1,5 +1,5 @@
 <?php
     include("../inc/fonctions.php");
-    achat_produit($quantite, $idproduit);
+    achat_produit($_POST['quantite_produit'], $_POST['id_produit']);
     header('location: Acceuil.php?numero_etu='.$_POST['numero_etu']);
 ?>
