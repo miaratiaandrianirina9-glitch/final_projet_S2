9ee2ca26d7f196296a8e09d17d1ee3dcae5efa65
vahinaaina-droid <vahinaainanirina@gmail.com>
2026-07-20 13:58:35 +0300

    miaratia changes

--- pages/Mes_Ventes.php
+++ pages/Mes_Ventes.php
@@ -1,6 +1,10 @@
 <?php 
     include_once("../inc/fonctions.php");
-    $montant=montant_vente_by_member($_GET['id_membre']);
+    if(!isset($_GET['id_membre'])){
+        header("Location: index.php");
+    }
+    $montant=get_montant_total_vente($_GET['id_membre']);
+    $produit_vendu=get_produit_vendu($_GET['id_membre']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,11 +12,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="body">
-    <p>Montant total: <?= $montant['montant_total']?></p>
-</div>
+    <p>Membre: <? echo $montant['nom_membre'];?></p>
+    <table border="1">
+        <tr>
+            <td>Produit</td>
+            <td>Quantite vendu</td>
+            <td>Prix de vente</td>
+            <td>Prix total</td>
+        </tr>
+        <? foreach($produit_vendu as $produit){?>
+            <tr>
+                <td><? echo $produit['nom_produit']; ?></td>
+                <td><? echo $produit['quantite_vendu']; ?></td>
+                <td><? echo $produit['prix_vente']; ?></td>
+                <td><? echo $produit['prix_total']; ?></td>
+            </tr>
+        <? }?>
+    </table>
+    <p>Montant total des ventes: <?= $montant['montant_total']?></p>
 </body>
 </html>
